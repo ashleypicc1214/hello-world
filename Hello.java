e542ebe160e7c3daecda95acdd44cc38781513ad
ashleypicc1214 <59940422+ashleypicc1214@users.noreply.github.com>
2020-01-15 17:58:39 -0500

    Lab 1

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/********************************************************************
+* Hello.java
+*********************************************************************
+* Ashley Picchiottino
+* 1/15/2020
+* CMSC 255-901
+********************************************************************/
+
+public class Hello {
+
+		public static void main(String[] args) {
+
+		System.out.println("<Ashley Picchiottino");
+		System.out.println("<Hello.java>");
+		System.out.println("<First Project>");
+		System.out.println("<1/15/2020>");
+		System.out.println("<CMSC 255-901>");
+		System.out.println();
+		}
+}
